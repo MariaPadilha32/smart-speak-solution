26cd8510420179263d9af47fd3ec53e90c1248f4
Maria Fernanda Padilha <mdpadilha90@gmail.com>
2024-07-16 20:32:40 +0000

    059 - dedployment part 5

--- smartspeaksolution/settings.py
+++ smartspeaksolution/settings.py
@@ -194,17 +194,17 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    #AWS_STORAGE_BUCKET_NAME = 'smartspeaksolutions'
-    #AWS_S3_REGION_NAME = 'eu-north-1'
-    #AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    #AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    #AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_STORAGE_BUCKET_NAME = 'smartspeaksolution'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    #STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    #STATICFILES_LOCATION = 'static'
-    #DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    # MEDIAFILES_LOCATION = 'media'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
     #STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
