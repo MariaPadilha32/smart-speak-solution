a3c5ad8d0a849d9433f6bcfa5340edb19713449a
Maria Fernanda Padilha <mdpadilha90@gmail.com>
2024-07-17 19:37:51 +0000

    amend css issues on blog

--- bag/contexts.py
+++ bag/contexts.py
@@ -17,20 +17,20 @@ def bag_contents(request):
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
-                #'quantity': item_data,
+                'quantity': item_data,
                 'product': product,
             })
-        #else:
-            #product = get_object_or_404(Product, pk=item_id)
-            #for size, quantity in item_data['items_by_size'].items():
-              #  total += quantity * product.price
-              #  product_count += quantity
-              #  bag_items.append({
-                #    'item_id': item_id,
-                    #'quantity': quantity,
-                  #  'product': product,
-                  #  'size': size,
-             #   })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -14,7 +14,7 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     product = get_object_or_404(Product, pk=item_id)
-    #quantity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
@@ -24,21 +24,21 @@ def add_to_bag(request, item_id):
     if size:
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += 1
+                bag[item_id]['items_by_size'][size] += quantity
                 messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
-                bag[item_id]['items_by_size'][size] = 1
+                bag[item_id]['items_by_size'][size] = quantity
                 messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
             messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
     else:
         if item_id in list(bag.keys()):
-            bag[item_id] += 1
+            bag[item_id] += quantity
             bag[name_client] = name_client
             messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
-            bag[item_id] = 1
+            bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
