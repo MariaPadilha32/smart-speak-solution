3f20ade37a1b8a99d53e5648350b83c920cc68bc
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-08-31 19:23:45 +0000

    change functionality from contact pages

--- contact/admin.py
+++ contact/admin.py
@@ -5,6 +5,6 @@ from .models import Contact
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     """Add contact filters to admin panel"""
-    list_display = ('email', 'subject', 'message', 'reason')
-    list_filter = ('email', 'reason')
-    search_fields = ('email', 'reason')
+    list_display = ('email', 'subject', 'message')
+    list_filter = ('email',)
+    search_fields = ('email',)
--- contact/forms.py
+++ contact/forms.py
@@ -12,7 +12,6 @@ class ContactForm(ModelForm):
             'email': 'Email',
             'subject': 'Subject',
             'message': 'Message',
-            'reason': 'Reason for contact',
         }
 
          self.fields['email'].widget.attrs['autofocus'] = True
@@ -28,7 +27,6 @@ class ContactGeneralForm(ModelForm):
             'email': 'Email',
             'subject' : 'Subject',
             'message': 'Message',
-            
         }
 
          self.fields['email'].widget.attrs['autofocus'] = True
