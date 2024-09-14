f7672c305dead92dd33d89a3b6f95e8b93c0ca6b
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-09-14 19:25:57 +0000

    len

--- gallery/views.py
+++ gallery/views.py
@@ -6,7 +6,7 @@ from .forms import GalleryForm
 def gallery(request):
     print('HEEEEREEEEE')
     photos = PhotosGallery.objects.all()
-    total = PhotosGallery.objects.__len__()
+    total = len(photos)
     #photos = Gallery.objects.all()
     #total = photos.count() # SELECT COUNT(*) FROM TALE
     #total = total.count
