e8b41d87b761621fe0f7c5511863589f8011169a
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-09-15 12:05:40 +0000

    register admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Catalog
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -23,6 +23,9 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
+class CatalogAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Catalog, CatalogAdmin)
