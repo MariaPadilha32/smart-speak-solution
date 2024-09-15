d724915967a117ba48cab63c16760e174916f11d
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-09-15 13:00:38 +0000

    new field

--- smartspeaksolution/settings.py
+++ smartspeaksolution/settings.py
@@ -235,4 +235,4 @@ else:
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
