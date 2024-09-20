c39966826c401560b6f02b380b51df8fdc3bef9e
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-09-20 20:45:41 +0000

    forms.pt newsletter

--- home/forms.py
+++ home/forms.py
@@ -4,9 +4,9 @@ from django import forms
 class NewsletterForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
-    recipients = forms.CharField(help_text='Comma-separated email addresses')
+    receivers = forms.CharField(widget=forms.Textarea, help_text='Comma-separated email addresses')  # Add receivers field
 
     def clean_recipients(self):
-        emails = self.cleaned_data['recipients']
+        emails = self.cleaned_data['receivers']
         emails_list = [email.strip() for email in emails.split(',')]
         return emails_list
