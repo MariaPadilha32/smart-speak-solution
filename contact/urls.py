c77dd1d2a8134768a77948c830e01e4dbc3560e4
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-09-21 18:16:22 +0000

    create messages views page

--- contact/urls.py
+++ contact/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.contactgeneral, name='contact general'),
     path('general/', views.contact, name='contact'),
     path('faq/', views.contactfaq, name='contactfaq'),
+    path('messages/', views.view_contact_messages, name='view_contact_messages')
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .forms import ContactForm, ContactGeneralForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import Contact
 
 
 def contact(request):
@@ -57,3 +59,18 @@ def contactgeneral(request):
 def contactfaq(request):
 
     return render(request, 'contact/contact-faq.html')
+
+
+def is_admin(user):
+    return user.is_superuser
+
+@login_required
+@user_passes_test(is_admin)
+def view_contact_messages(request):
+    """Admin view to display all contact messages"""
+    contacts = Contact.objects.all().order_by('-id')  
+    template = 'contact/contact_messages.html'
+    context = {
+        'contacts': contacts
+    }
+    return render(request, template, context)
